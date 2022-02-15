6479e818a5a2e1eab1d9e49be98b87a3717b03ae
Cameron Leathers <99282208+CameonTL@users.noreply.github.com>
2022-02-15 12:42:27 -0600

    Create ProgramAssignment03.py

---
+++ ProgramAssignment03.py
@@ -0,0 +1,13 @@
+from pets import Pets
+
+def main():
+  name = input('Enter your pet\'s name: ')
+  animalType = input('Enter what kind of animal your pet is: ')
+  age = int(input('Enter your pet\'s age: '))
+  
+  animalInfo = Pets(name, animalType, age)
+  
+  print('Here is the information you entered about your pet:')
+  print(animalInfo)
+  
+main()
